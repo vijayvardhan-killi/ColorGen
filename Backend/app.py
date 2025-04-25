14fe494c4faf474bcadce95e775573accbbba016
Vijay vardhan <vijayvardhan.killi@gmail.com>
2025-04-25 12:05:22 +0530

    Fixed CORS

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask , jsonify , request
+from flask_cors import CORS
 from helpers import generate_color_palette, format_palette
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
